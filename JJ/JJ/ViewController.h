84c445be3a363ff9e223f3ee1c882f416075a26c
paladin <wangjianqi@aliyun.com>
2018-04-01 20:35:36 +0800

    add jj by objective-c

---
+++ JJ/JJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJ
+//
+//  Created by 王建旗 on 2018/4/1.
+//  Copyright © 2018年 JQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JJ/JJ/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  JJ
+//
+//  Created by 王建旗 on 2018/4/1.
+//  Copyright © 2018年 JQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    printf(@"hello world");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
